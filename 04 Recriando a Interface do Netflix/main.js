516f4e91e5528880330d961fcac6d66c03b61da5
marceldonin <mdonin@gmail.com>
2021-07-06 22:39:08 -0300

    Netflix Marvel

---
+++ 04 Recriando a Interface do Netflix/main.js
@@ -0,0 +1,19 @@
+/*
+Theme Name: 
+Theme URI: 
+Author: Marcel Donin
+Author URI: 
+Description: 
+Version: 1.0
+
+--------------------------------------------
+ESTRUTURA BASE
+--------------------------------------------*/
+/***** DECLARAÇÃO DE VARIAVEIS*/
+/***** ESTILOS GERAIS*/
+/***** BODY*/
+/***** HEADER*/
+/***** FOOTER*/
+/***** PÁGINA INICIAL*/
+/***** EXTRAS*/
+/***** @MEDIA*/
