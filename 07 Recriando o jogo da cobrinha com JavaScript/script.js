391c2148c704097bf18e6d57bb6c523f5ca31d02
marceldonin <mdonin@gmail.com>
2021-07-17 19:28:26 -0300

    foi definido os pontos aleatórios da comida e o tamanho da cobrinha

--- 07 Recriando o jogo da cobrinha com JavaScript/script.js
+++ 07 Recriando o jogo da cobrinha com JavaScript/script.js
@@ -79,12 +79,19 @@ function iniciarJogo(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    
 
     let newHead = {
         x: snakeX,
@@ -98,9 +105,5 @@ let jogo = setInterval(iniciarJogo, 100);
 
 
 
-
-/***** HEADER*/
-/***** FOOTER*/
-/***** PÁGINA INICIAL*/
 /***** EXTRAS*/
-/***** @MEDIA*/
+
