0d927b71605fdb682d502ee8871105b74c7717e1
Marcel Donin <83168668+marceldonin@users.noreply.github.com>
2023-11-12 10:00:05 -0300

    Desafio04MarvelFlix Atualizado para execução

---
+++ Desafio04MarvelFlix.js
@@ -0,0 +1,41 @@
+/*
+Theme Name: 
+Theme URI: 
+Author: Marcel Donin
+Author URI: 
+Description: Clone Netflix
+Data: Julho/2021 
+Version: 1.0
+
+--------------------------------------------
+ESTRUTURA BASE
+--------------------------------------------*/
+
+/* JQUERY OWL */
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+/***** DECLARAÇÃO DE VARIAVEIS*/
+/***** ESTILOS GERAIS*/
+/***** BODY*/
+/***** HEADER*/
+/***** FOOTER*/
+/***** PÁGINA INICIAL*/
+/***** EXTRAS*/
+/***** @MEDIA*/
